bfcddc948dbc429796027e0803bb80db1cea6d67
Elias Neerbye <140179536+BlankTuber@users.noreply.github.com>
2024-11-08 09:53:57 +0100

    Removed unused routes

--- website/routes/index.js
+++ website/routes/index.js
@@ -5,19 +5,4 @@ router.get("/", (req, res) => {
     res.render("index");
 });
 
-router.get("/discord", (req, res) => {
-    const error = req.query.error;  // Retrieve error from query if present
-    res.render("login", { error });
-});
-
-router.post("/discord", (req, res) => {
-    const { code } = req.body;
-    if (code === process.env.ACCESS_CODE) {
-        res.render("discord");
-    } else {
-        // Pass error as a query parameter in the redirect
-        res.redirect("/discord?error=Not authorized");
-    }
-});
-
 module.exports = router;
